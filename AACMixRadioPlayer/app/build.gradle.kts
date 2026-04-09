63a8e7f4524c1d71e35f79038bdca9d6190c66b6
pavangothe1 <pavangothe@nextgen.com>
2026-04-09 21:25:52 +0530

    v1.0.46: UI improvements, Android Auto fixes, splashing screen & dynamic theming

--- AACMixRadioPlayer/app/build.gradle.kts
+++ AACMixRadioPlayer/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.pypyradio.aacplayer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 45
-        versionName = "1.0.45"
+        versionCode = 46
+        versionName = "1.0.46"
     }
 
     signingConfigs {
