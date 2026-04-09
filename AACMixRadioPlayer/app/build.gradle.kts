75d1a4a9df6acf02f192c3e08e28d293d7bc884a
pavangothe1 <pavangothe@nextgen.com>
2026-04-09 23:36:53 +0530

    1.0.55UX: completely hide known unplayable stations from the list instead of marking them with a warning

--- AACMixRadioPlayer/app/build.gradle.kts
+++ AACMixRadioPlayer/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.pypyradio.aacplayer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 54
-        versionName = "1.0.54"
+        versionCode = 55
+        versionName = "1.0.55"
     }
 
     signingConfigs {
