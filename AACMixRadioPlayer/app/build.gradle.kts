c85154eabfae56cb51851f5e984b195b18453524
pypycodes <pypycodess@gmail.com>
2026-04-09 20:50:25 +0530

    v1.0.45: UI improvements, Android Auto fixes, splashing screen & dynamic theming

--- AACMixRadioPlayer/app/build.gradle.kts
+++ AACMixRadioPlayer/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.pypyradio.aacplayer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 44
-        versionName = "1.0.44"
+        versionCode = 45
+        versionName = "1.0.45"
     }
 
     signingConfigs {
