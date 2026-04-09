28b280523cc3f7f226ee3f30848559f4c1189446
pypycodes <pypycodess@gmail.com>
2026-04-09 23:00:20 +0530

    Fix compile warnings in AppRoot and FullScreenPlayer

--- AACMixRadioPlayer/app/build.gradle.kts
+++ AACMixRadioPlayer/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.pypyradio.aacplayer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 50
-        versionName = "1.0.50"
+        versionCode = 51
+        versionName = "1.0.51"
     }
 
     signingConfigs {
