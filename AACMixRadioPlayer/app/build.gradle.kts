704d1eea9e19f7f4471f178b934475633733cc0b
pypycodes <pypycodess@gmai.com>
2026-04-09 06:46:45 +0000

    1.0.37 new verison

--- AACMixRadioPlayer/app/build.gradle.kts
+++ AACMixRadioPlayer/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.pypyradio.aacplayer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 36
-        versionName = "1.0.36"
+        versionCode = 37
+        versionName = "1.0.37"
     }
 
     signingConfigs {
