c185c3c6c8b09df5980c0f0f970fbc81683877f0
pavangothe1 <pavangothe@nextgen.com>
2026-04-12 22:00:54 +0530

    Add onGetSession logging to diagnose Android Auto connection

--- AACMixRadioPlayer/app/build.gradle.kts
+++ AACMixRadioPlayer/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.pypyradio.aacplayer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 84
-        versionName = "1.0.84"
+        versionCode = 85
+        versionName = "1.0.85"
     }
 
     signingConfigs {
