3bc990d3bac79d7450f4a4e81b66056b05ccfbb9
pavangothe1 <pavangothe@nextgen.com>
2026-04-15 22:47:57 +0530

    feat: v2.0.4 fixes

--- AACMixRadioPlayer/app/build.gradle.kts
+++ AACMixRadioPlayer/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.pypyradio.aacplayer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 103
-        versionName = "2.0.3"
+        versionCode = 104
+        versionName = "2.0.4"
     }
 
     signingConfigs {
