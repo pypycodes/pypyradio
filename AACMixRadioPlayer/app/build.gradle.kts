7b1038ede06a1d28216a4f4305d35555a2e43dca
pypycodes <pypycodess@gmail.com>
2026-04-15 19:35:20 +0530

    chore: minor fixes

--- AACMixRadioPlayer/app/build.gradle.kts
+++ AACMixRadioPlayer/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.pypyradio.aacplayer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 96
-        versionName = "1.0.96"
+        versionCode = 97
+        versionName = "1.0.97"
     }
 
     signingConfigs {
